72eb48bd07cc676a59bd24e37acb27ea5668ab16
Hussein-shsx3 <husseinshsx3@gmail.com>
2024-10-30 21:42:22 +0200

    start in header

--- client/src/Components/header.jsx
+++ client/src/Components/header.jsx
@@ -1,52 +1,228 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../Api/userApi";
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+const products = [
+  {
+    name: "Web Developement",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Data Sciense",
+    description: "Speak directly to your customers",
+    href: "#",
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: "Mobile Developement",
+    description: "Your customers’ data will be safe and secure",
+    href: "#",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Programing Languages",
+    description: "Connect with third-party tools",
+    href: "#",
+    icon: SquaresPlusIcon,
+  },
+];
 
 const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const user = useSelector((state) => state.user.user);
+  console.log(user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
   return (
-    <header className="w-full px-4 py-6 flex items-center bg-background">
-      <img className="w-[110px]" src="./images/logo-udemy.svg" alt="" />
-      <button className="items-center hidden mx-5 md:flex">
-        <i className="bx bxs-category text-primary text-[15px]"></i>
-        Categories
-      </button>
-      <div className="hidden md:flex items-center gap-3 w-[50%] border-[1px] border-black px-3 py-3 rounded-3xl bg-gray-50">
-        <i className="bx bx-search text-[20px] text-text"></i>
-        <input
-          type="text"
-          placeholder="Search for courses"
-          className="w-[80%] bg-transparent outline-none"
-        />
-      </div>
-      <nav className="absolute  md:relative text-[15px] items-center justify-between flex mx-5 w-[30%]">
-        <Link to="" className="link">
-          Home
-        </Link>
-        <Link to="" className="link">
-          Udemy Courses
-        </Link>
-        <Link to="" className="link">
-          My Courses
-        </Link>
-        <i className="bx bx-heart text-[25px]"></i>
-        <i className="bx bx-cart-alt text-[25px]"></i>
-        <i className="bx bx-bell text-[25px]"></i>
-        {false ? (
-          <img
-            src="./images/profile-photo.png"
-            alt=""
-            className="w-[36px] h-[36px] rounded-full"
-          />
-        ) : (
-          <span className="bg-black text-white flex justify-center items-center font-semibold w-[36px] h-[36px] rounded-full">
-            H
-          </span>
-        )}
+    <header className="bg-white w-full">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex w-[95%] items-center justify-between p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <a href="." className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img alt="" src="./images/logo.png" className="h-7 w-auto" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
+        </div>
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <PopoverButton className="flex items-center gap-x-1 font-semibold text-gray-900">
+              Categories
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="h-5 w-5 flex-none text-gray-400"
+              />
+            </PopoverButton>
+
+            <PopoverPanel
+              transition
+              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+            >
+              <div className="p-4">
+                {products.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
+                  >
+                    <div className="flex-auto">
+                      <a
+                        href={item.href}
+                        className="block font-semibold text-gray-900"
+                      >
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </PopoverPanel>
+          </Popover>
+          <Link to="/" className="font-semibold text-gray-900">
+            Home
+          </Link>
+          <Link to="#" className="font-semibold text-gray-900">
+            All Courses
+          </Link>
+          <Link to="#" className="font-semibold text-gray-900">
+            My Courses
+          </Link>
+        </PopoverGroup>
+        <div className="hidden lg:flex items-center gap-3 lg:flex-1 lg:justify-end">
+          <button className="bg-primary text-white px-5 py-2 rounded-[20px]">
+            Enroll Now
+          </button>
+          {user !== null ? (
+            <Link to="." className="">
+              <img
+                src={user.picture ? user.picture : "./images/profile-photo.png"}
+                alt=""
+                className="w-[38px] h-[38px] rounded-full"
+              />
+            </Link>
+          ) : (
+            <Link
+              to=""
+              className="bg-primary text-white px-5 py-2 rounded-[20px]"
+            >
+              Login
+            </Link>
+          )}
+        </div>
       </nav>
-      <i className="bx bx-search text-[20px] text-title flex md:hidden"></i>
-      <div className="relative">
-        <i className="bx bx-cart-alt text-[25px]"></i>
-        <span>1</span>
-      </div>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="." className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img alt="" src="./images/logo.png" className="h-5 w-auto" />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                    Categories
+                    <ChevronDownIcon
+                      aria-hidden="true"
+                      className="h-5 w-5 flex-none group-data-[open]:rotate-180"
+                    />
+                  </DisclosureButton>
+                  <DisclosurePanel className="mt-2 space-y-2">
+                    {[...products].map((item) => (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ))}
+                  </DisclosurePanel>
+                </Disclosure>
+                <Link
+                  to=""
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Home
+                </Link>
+                <Link
+                  to=""
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  All Courses
+                </Link>
+                <Link
+                  to=""
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  My Courses
+                </Link>
+              </div>
+              <div className="py-6">
+                <a
+                  href="."
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
     </header>
   );
 };
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,10 +3,9 @@ import Header from "../Components/header";
 
 const Home = () => {
   return (
-    <section className="relative w-full min-h-[100dvh] flex flex-col items-center overflow-hidden bg-gray-200">
+    <section className="relative w-full min-h-[100dvh] flex flex-col items-center overflow-hidden">
       <Header />
       <div className="container w-full flex flex-col items-center px-2 md:px-0">
-        kyuk
       </div>
     </section>
   );
